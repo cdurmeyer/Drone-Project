4ea0fbeccc0c202e10f5ede2a2492249af67eab4
MS CSLab <mscslab@woodward.edu>
2022-04-08 13:41:15 -0400

    added package.json

---
+++ README.md
@@ -0,0 +1,3 @@
+# drone Project
+## description
+I want to try to fly the drone in a certain space, and get it to work. Also, I want to program it.
---
+++ telloChristian.js
@@ -0,0 +1,12 @@
+// variable for passing into function
+var msg = "Mr. Stewart says ";
+// function to print to console/terminal
+function helloworld(msg)
+{console.log(msg + "hello world")}
+helloworld(msg)
+
+
+
+
+
+
